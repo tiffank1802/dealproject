36437af0448e86e64924f48d615100fc5042da03
tiffank1802 <tiffank1802@gmail.com>
2026-07-31 23:33:51 +0000

    premier projet deal.II

---
+++ build/CMakeFiles/step-1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-1.dir/step-1.cc.o"
+  "CMakeFiles/step-1.dir/step-1.cc.o.d"
+  "step-1"
+  "step-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
